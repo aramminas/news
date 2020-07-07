d220be04d73eb385ed348ba18d316652c74324e1
Aram <armfiletransfer@gmail.com>
2020-07-07 22:33:12 +0400

    test commit

---
+++ precache-manifest.cd61d4e7daa16106ea9a11a61c786078.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c71e92148ae1b190c974b65bc31bfcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfa260164045daa202dd",
+    "url": "/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "6d9abf2b15ea6fa06518",
+    "url": "/static/js/2.42d719e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42d719e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa260164045daa202dd",
+    "url": "/static/js/main.b5b0a6c0.chunk.js"
+  },
+  {
+    "revision": "82af36e83412180715d7",
+    "url": "/static/js/runtime-main.86c19a1b.js"
+  }
+]);
